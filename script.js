f7bfdb424002a361fe7a2ef59a67dfb0ec4ee5e7
Audrey Wong <audreywong@Audreys-iMac.local>
2021-08-04 08:55:23 -0700

    make cancel-btn functional with JS

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+const pg1 = document.getElementById("main-options");
+const pg2 = document.getElementById("pg2-options");
+
+// next page button
 const next = document.getElementById("next-btn");
 next.addEventListener("click", nextPage);
 
 function nextPage() {
-	const pg1 = document.getElementById("main-options");
-	const pg2 = document.getElementById("pg2-options");
-
 	pg1.style.display = "none";
 	pg2.style.display = "block";
+}
+
+// cancel button
+const cancel = document.getElementById("cancel");
+cancel.addEventListener("click", stopAction);
+
+function stopAction() {
+	pg1.style.display = "block";
+	pg2.style.display = "none";
 }
