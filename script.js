0c0301fefdbe709e1a64e3ec6235b560d9ada990
David Tarrab <tarrabd@ramaz.org>
2021-08-10 12:39:06 -0400

    Finished button stuff for real this time

--- script.js
+++ script.js
@@ -2,73 +2,59 @@ const actions = ["1", "2", "3", "4", "5", "6", "7", "8"];
 
 var buttonValues = [];
 
-const moreActions = [["1-1", "1-2", "1-3", "1-4", "1-5", "1-6", "1-7", "1-8"], ["2-1", "2-2", "2-3", "2-4", "2-5", "2-6", "2-7", "2-8"], ["3-1", "3-2", "3-3", "3-4", "3-5", "3-6", "3-7", "3-8"], ["4-1", "4-2", "4-3", "4-4", "4-5", "4-6", "4-7", "4-8"]];
-
-var setNum = 0;
+const moreActions = [["1-1", "1-2", "1-3", "1-4", "1-5", "1-6", "1-7", "1-8"], ["2-1", "2-2", "2-3", "2-4", "2-5", "2-6", "2-7", "2-8"], ["3-1", "3-2", "3-3", "3-4", "3-5", "3-6", "3-7", "3-8"], ["4-1", "4-2", "4-3", "4-4", "4-5", "4-6", "4-7", "4-8"], ["5-1", "5-2", "5-3", "5-4", "5-5", "5-6", "5-7", "5-8"], ["6-1", "6-2", "6-3", "6-4", "6-5", "6-6", "6-7", "6-8"], ["7-1", "7-2", "7-3", "7-4", "7-5", "7-6", "7-7", "7-8"], ["8-1", "8-2", "8-3", "8-4", "8-5", "8-6", "8-7", "8-8"]];
 
 const buttons = [document.getElementById("btn1"), document.getElementById("btn2"), document.getElementById("btn3"), document.getElementById("btn4")];
 const cancelButton = document.getElementById("cancel-btn");
 const nextButton = document.getElementById("next-btn");
 
+var setNum = 0;
+var subNum = 0;
+const maxSetNum = actions.length/buttons.length -1;
+
 const convertToValues = [
   function() {
-    for (var i=0; i<=buttonValues.length/buttons.length-1; i++) {
-      if (setNum == i) {
-        if (buttonValues == actions) {
-          setMoreOptions[i*buttons.length+0]();
-          setButtonValues();
-        }
-        if (buttonValues == moreActions[0]) {
-          console.log(buttons[0].value);
-        }
-      }
+    if (buttonValues == moreActions[subNum]) {
+      console.log(buttons[0].value);
+    }
+    if (buttonValues[0+setNum*buttons.length] == actions[0+setNum*buttons.length]) {
+      subNum = 0+setNum*buttons.length;
+      setMoreOptions();
     }
   },
   function() {
-    for (var i=0; i<=buttonValues.length/buttons.length-1; i++) {
-      if (setNum == i) {
-        if (buttonValues == actions) {
-          setMoreOptions[i*buttons.length+1]();
-          setButtonValues();
-        }
-        if (buttonValues == moreActions[1]) {
-          console.log(buttons[1].value);
-        }
-      }
+    if (buttonValues == moreActions[subNum]) {
+      console.log(buttons[1].value);
+    }
+    if (buttonValues[1+setNum*buttons.length] == actions[1+setNum*buttons.length]) {
+      subNum = 1+setNum*buttons.length;
+      setMoreOptions();
     }
   },
   function() {
-    for (var i=0; i<=buttonValues.length/buttons.length-1; i++) {
-      if (setNum == i) {
-        if (buttonValues == actions) {
-          setMoreOptions[i*buttons.length+2]();
-          setButtonValues();
-        }
-        if (buttonValues == moreActions[2]) {
-          console.log(buttons[2].value);
-        }
-      }
+    if (buttonValues == moreActions[subNum]) {
+      console.log(buttons[2].value);
+    }
+    if (buttonValues[2+setNum*buttons.length] == actions[2+setNum*buttons.length]) {
+      subNum = 2+setNum*buttons.length;
+      setMoreOptions();
     }
   },
   function() {
-    for (var i=0; i<=buttonValues.length/buttons.length-1; i++) {
-      if (setNum == i) {
-        if (buttonValues == actions) {
-          setMoreOptions[i*buttons.length+3]();
-          setButtonValues();
-        }
-        if (buttonValues == moreActions[3]) {
-          console.log(buttons[3].value);
-        }
-      }
+    if (buttonValues == moreActions[subNum]) {
+      console.log(buttons[3].value);
+    }
+    if (buttonValues[3+setNum*buttons.length] == actions[3+setNum*buttons.length]) {
+      subNum = 3+setNum*buttons.length;
+      setMoreOptions();
     }
   },
 ];
 
 function setButtonValues() {
   for (var i=0; i<buttons.length; i++) {
-    buttons[i].innerHTML = buttonValues[i];
-    buttons[i].value = buttonValues[i];
+    buttons[i].innerHTML = buttonValues[i+setNum*buttons.length];
+    buttons[i].value = buttonValues[i+setNum*buttons.length];
   }
 }
 
@@ -78,36 +64,20 @@ function setActions() {
   setButtonValues();
 }
 
-const setMoreOptions = [
-  function() {
-    buttonValues = moreActions[0];
-    setButtonValues();
-  },
-  function() {
-    buttonValues = moreActions[1];
-    setButtonValues();
-  },
-  function() {
-    buttonValues = moreActions[2];
-    setButtonValues();
-  },
-  function() {
-    buttonValues = moreActions[3];
-    setButtonValues();
-  }
-];
+function setMoreOptions() {
+  buttonValues = moreActions[subNum];
+  setNum = 0;
+  setButtonValues();
+}
 
 function nextSet() {
   setNum++;
 
-  if (setNum > buttonValues.length/buttons.length -1) {
+  if (setNum > maxSetNum) {
     setNum = 0;
   }
 
-  for (var i=0; i<buttons.length; i++) {
-    buttons[i].innerHTML = buttonValues[setNum*buttons.length+i];
-    buttons[i].value = buttonValues[setNum*buttons.length+i];
-  }
+  setButtonValues();
 }
 
 cancelButton.addEventListener("click", setActions);
